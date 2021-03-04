c468011503d2462602caa197e0e19d994496e7d2
ikalyzhny <illia.kaliuzhnyi@freshcodeit.net>
2021-03-04 10:42:28 +0200

    feat: shared preferences repository was added

---
+++ app/src/main/java/com/e/zguide/repositories/SharedPreferencesRepository.java
@@ -0,0 +1,37 @@
+package com.e.zguide.repositories;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPreferencesRepository {
+    public static final String SHARED_PREFERENCES_KEY = "SHARED_PREFERENCES";
+    public static final String IS_VISITED = SHARED_PREFERENCES_KEY + "_IS_VISITED";
+
+
+    private SharedPreferences mPreferences;
+    private static SharedPreferencesRepository instance;
+
+    protected SharedPreferencesRepository(SharedPreferences preferences) {
+        this.mPreferences = preferences;
+    }
+
+    public static SharedPreferencesRepository getInstance(Context context) {
+        if (instance == null) {
+            instance = new SharedPreferencesRepository(context.getSharedPreferences(SHARED_PREFERENCES_KEY, Context.MODE_PRIVATE));
+        }
+
+        return instance;
+    }
+
+    public static SharedPreferencesRepository getInstance() {
+        return instance;
+    }
+
+    public boolean getIsVisited() {
+        return mPreferences.getBoolean(IS_VISITED, false);
+    }
+
+    public boolean setIsVisited(Boolean value) {
+        return mPreferences.edit().putBoolean(IS_VISITED, value).commit();
+    }
+}
