2c0c4ce4c03c19a18706316477a67dd4ae3c361f
ikalyzhny <illia.kaliuzhnyi@freshcodeit.net>
2021-03-24 10:43:23 +0200

    feat: button listeners were added

--- app/src/main/java/com/e/zguide/fragments/PlacesFragment/PlacesFragment.java
+++ app/src/main/java/com/e/zguide/fragments/PlacesFragment/PlacesFragment.java
@@ -2,13 +2,17 @@ package com.e.zguide.fragments.PlacesFragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.e.zguide.R;
+import com.e.zguide.databinding.FragmentPlacesBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,51 +20,34 @@ import com.e.zguide.R;
  * create an instance of this fragment.
  */
 public class PlacesFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FragmentPlacesBinding binding;
+    NavController navController;
 
     public PlacesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PlacesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PlacesFragment newInstance(String param1, String param2) {
-        PlacesFragment fragment = new PlacesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static PlacesFragment newInstance() {
+        return new PlacesFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_places, container, false);
+        binding = FragmentPlacesBinding.inflate(inflater, container, false);
+        navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+
+        binding.btnGoToFavorites.setOnClickListener(v -> navController.navigate(R.id.favoritesFragment));
+
+        binding.btnGoToSearch.setOnClickListener(v -> navController.navigate(R.id.searchFragment));
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
